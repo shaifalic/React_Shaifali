2ab7ca12d4a6beb3a9c765d60c3a7fa4e16d2b69
Shaifali Choudhary <shaifali.choudhary@fareportal.com>
2022-02-15 23:18:43 +0530

    Day1

---
+++ Employee.js
@@ -0,0 +1,17 @@
+function EmployeeDetails() {
+    var name= "Shaifali";
+    var age = 20;
+    var designation = "SDE";
+    
+    return {
+      name: name,
+      age: age,
+      designation: designation
+    }
+  }
+  
+  var newEmployee = EmployeeDetails()
+  
+  var userName = newEmployee.name;
+  var userAge = newEmployee.age;
+  var userDesignation = newEmployee.designation;
