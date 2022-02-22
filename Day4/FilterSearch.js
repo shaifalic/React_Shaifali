41a8f7da56785dfb221478fcd5f85e4412bd2cf7
Shaifali Choudhary <shaifali.choudhary@fareportal.com>
2022-02-22 22:49:49 +0530

    Day4

--- Day4/FilterSearch.js
+++ Day4/FilterSearch.js
@@ -5,8 +5,8 @@ export default function FilterSearch(props){
     var [searchTerm, setSearchTerm]=useState("");
     return (
         <div>
-            <h2><center>Employee List</center></h2>
-            <input type="text" placeholder="Search..." value={searchTerm} onChange={(event)=>{setSearchTerm(event.target.value);}}/>
+            <h2><center>Employee List</center></h2><br/>
+            <p><center><input type="text" placeholder="Search..." value={searchTerm} onChange={(event)=>{setSearchTerm(event.target.value);}}/></center></p>
             {props.empList.map((val)=>{
                 if(searchTerm==""){
                     return <EmployeeDetailsComponent {...val}></EmployeeDetailsComponent>
