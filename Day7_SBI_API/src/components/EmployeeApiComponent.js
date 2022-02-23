7efe2fbd86b3bc9e0ff65b1f686edbd948707cad
Shaifali Choudhary <shaifali.choudhary@fareportal.com>
2022-02-23 20:21:39 +0530

    Day7_SBI_API

---
+++ Day7_SBI_API/src/components/EmployeeApiComponent.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Axios from "axios"
+import EmployeeDetailsComponent from "./EmployeeDetailsComponent"
+
+export default class EmployeeApiComponent extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            employeeList: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Employee List is given Below:</h2>
+                {this.state.employeeList.map((employee) => {
+                    return <EmployeeDetailsComponent key={employee.eid} {...employee}></EmployeeDetailsComponent>
+                })}
+            </div>
+        )
+    }
+
+    deleteEmployee = (event) => {
+        debugger;
+        Axios.delete("https://localhost:5001/api/Employee" + event.target.id).then(() => {
+            alert("Employee is Deleted");
+            this.getData();
+        })
+    }
+
+    getData = () => {
+        var dataPromise = Axios.get("https://localhost:5001/api/Employee");
+        dataPromise.then((response) => {
+            this.setState({
+                employeeList: response.data
+            })
+        })
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+}
---
+++ Day7_SBI_API/src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import App from "./components/SbiApiComponent";
+//import employeeList from "./Data";
+
+ReactDOM.render(<App ></App>,document.getElementById("root"))
